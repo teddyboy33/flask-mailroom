3d5bb084c6781d2c4ee7d8d310a79059e30486aa
Philip Korte <kortephilip@gmail.com>
2020-04-06 20:15:15 -0700

    Just commented out a line

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from model import Donation, Donor
 
 app = Flask(__name__)
-app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+# app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
 
 
 @app.route('/')
